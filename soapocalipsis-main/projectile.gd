d07893429cd04c995e9022b3d88b480cc1c57b30
Straver00 <macuino.555@gmail.com>
2025-01-25 23:30:52 -0500

    xd

--- soapocalipsis-main/projectile.gd
+++ soapocalipsis-main/projectile.gd
@@ -7,3 +7,12 @@ func _ready():
 	$AnimatedSprite2D.play("default")
 	await get_tree().create_timer(lifetime).timeout
 	queue_free()  # Elimina el proyectil después del tiempo defini
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Player":  # Asegúrate de que el nodo del personaje se llame "Player"
+		print("El personaje tocó el proyectil. Reiniciando el nivel...")
+		restart_level()
+		
+func restart_level():
+	get_tree().reload_current_scene()
