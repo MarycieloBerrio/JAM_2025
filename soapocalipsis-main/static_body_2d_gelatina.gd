7cc37b4165f2e2b06bb3994a849c94a9d3a8a75a
Straver00 <macuino.555@gmail.com>
2025-01-26 08:37:18 -0500

    new background

--- soapocalipsis-main/static_body_2d_gelatina.gd
+++ soapocalipsis-main/static_body_2d_gelatina.gd
@@ -1,5 +1,5 @@
 extends StaticBody2D
-@export var repulsion_force: float = 300
+@export var repulsion_force: float = 200
 
 func repel_player(player: CharacterBody2D) -> void:
 	var direction = (player.global_position - global_position).normalized()
